aeff1cb8ececa16e11b7d2b5d17e82e8c62fa6e4
Matthijs <gitlab@matth-ijs.nl>
2019-01-14 21:09:54 +0100

    Likert scales (semi-)work

---
+++ _plugins/likert.rb
@@ -0,0 +1,76 @@
+require 'securerandom'
+
+module Jekyll
+  class Likert < Liquid::Block
+    alias_method :render_block, :render
+
+    def initialize(tag_name, markup, tokens)
+      @markup = markup
+      super
+    end
+
+    # Uses the default Jekyll markdown parser to
+    # parse the contents of this block
+    #
+    def render(context)
+      _output = ""
+      text = super
+      site = context.registers[:site]
+      converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
+
+      # Render any liquid variables
+      markup = Liquid::Template.parse(@markup).render(context)
+
+      # Extract tag attributes
+      attributes = {}
+      markup.scan(Liquid::TagAttributes) do |key, value|
+        attributes[key] = value
+      end
+
+      @name = attributes["name"].gsub!(/\A"|"\Z/, '')
+
+      _output += "<div class='question'>"
+
+      @likerts = ""
+      i = 0
+      @nLikert = 0
+      text.lines.each do |line|
+        # if the line starts with [] or [.] or [ ] this must be turned into a radiobutton
+        # we then get the id, which is the part after [] in ()
+        if line =~ /^[\[X|\]]/
+          @nLikert += 1
+          # if @nLikert == 1
+          #   _output += '<div class=\'likert\'>'
+          # end
+          id = line[/[\(](.*)[\)]/,1]
+          @likerts += "<span class='likert_item'><input type='radio' id='" + id + @name + "' "
+
+          unless @name.nil?
+            @likerts += "name='" + @name + "' "
+          end
+
+          if line[1] == "X"
+            @likerts += "checked"
+          end
+
+          @likerts += ">"
+          # _output += "<label>" + line + "</label>"
+          @likerts += '<br><label for="' + id + @name + '">'
+          @likerts += converter.convert(line[/[\)](.*)/,1]).gsub(/<\/?p[^>]*>/, "")
+          @likerts += "</label></span>"
+        else
+          _output += converter.convert(line)
+        end
+        i += 1
+      end
+
+      _output += "<div class='likert'>" + @likerts + "</div>"
+
+      # _output += "</div><style>." + @guid + " { width:" + (100 / @nLikert).to_s + "%;}</style>"
+
+      _output + "</div>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('likert', Jekyll::Likert)
